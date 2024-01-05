dd87455314699c012f23a4f9906cc778dfa9cdd0
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-05 19:19:02 +0100

    UILayout component

---
+++ src/components/UILayout.vue
@@ -0,0 +1,55 @@
+<template>
+    <slot name="header"></slot>
+</template>
+
+<style scoped>
+:slotted(header,
+.resume,
+.movements) {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    padding: 14px 0;
+    box-sizing: border-box;
+}
+
+:slotted(header) {
+    position: fixed;
+    width: 100vw;
+}
+
+:slotted(.resume) {
+    min-height: 100vh;
+}
+
+:slotted(.movements) {
+    z-index: 1;
+    position: absolute;
+    flex-direction: column;
+    bottom: 0;
+    width: 100vw;
+    background-color: white;
+    box-shadow: 0 -8px 16px #e5e5e5;
+    border-radius: 24px;
+}
+
+:slotted(.movements .head) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 24px;
+    width: 100%;
+    box-sizing: border-box;
+}
+
+:slotted(.movements .body) {
+    height: 75vh;
+    width: 100%;
+}
+
+:slotted(.movements .head .grip) {
+    width: 120px;
+    height: 8px;
+    background-color: #e5e5e5;
+    border-radius: 4px;
+}</style>
