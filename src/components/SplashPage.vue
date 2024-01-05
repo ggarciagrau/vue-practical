fb7433a4f5fed5444cb905ba0d9e19ce23c077ac
Gerard Garcia Grau <gerardgarciagrau2@gmail.com>
2024-01-05 18:45:20 +0100

    SplashPage component

---
+++ src/components/SplashPage.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+    <div class="splashscreen">
+        <img src="../assets/cashflow-icon.svg" alt="logo" />
+        <h1><span>Splash</span>page</h1>
+    </div>
+</template>
+
+<style scoped>
+.splashscreen {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+}
+
+h1 {
+    color: var(--brand-blue);
+}
+
+h1 span {
+    color: var(--brand-green);
+}
+</style>
